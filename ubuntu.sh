5d0c602f6403ef4aede48eb5cf957bd37babda35
ichristmas <christmas.hack9@gmail.com>
2019-04-23 22:59:25 +0700

    add nes game

--- ubuntu.sh
+++ ubuntu.sh
@@ -12,7 +12,7 @@ sudo apt -y autoremove
 # general program #
 ###################
 
-sudo apt install -y aptitude figlet kazam vlc keepass2 ffmpeg unrar unzip htop traceroute dkms gnome-system-tools acpitool curl jq lm-sensors lolcat cmatrix p7zip-full xpad net-tools gnome-tweak-tool xclip xfce4-terminal
+sudo apt install -y aptitude figlet kazam vlc keepass2 ffmpeg unrar unzip htop traceroute dkms gnome-system-tools acpitool curl jq lm-sensors lolcat cmatrix p7zip-full xpad net-tools gnome-tweak-tool xclip xfce4-terminal nestopia
 sudo apt-get install -y --no-install-recommends gnome-panel
 # gnome-desktop-item-edit --create-new ~/Desktop
 
