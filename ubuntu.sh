5564a216277db34e73646f90e0bf9c7ed113add6
ichristmas <christmas.hack9@gmail.com>
2019-05-03 10:29:25 +0700

    add ngrok

--- ubuntu.sh
+++ ubuntu.sh
@@ -48,6 +48,15 @@ sudo apt install -y hub
 
 
 ### ----------
+# install ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+rm -rf ngrok-stable-linux-amd64.zip
+sudo mv ngrok /usr/local/bin
+#----------###
+
+
+### ----------
 # config vim
 cat <<EOF >> ~/.vimrc
 set nu
