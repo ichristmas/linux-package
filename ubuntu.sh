631f71f0f674396b569484d1a3bb9d28e7d1d8a3
ichristmas <christmas.hack9@gmail.com>
2020-08-23 19:26:33 +0700

    [Added] key sleep

--- ubuntu.sh
+++ ubuntu.sh
@@ -525,3 +525,6 @@ echo "
 xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 xrandr --addmode LVDS-1 "1920x1080_60.00"
 " >> ~/.profile
+
+# key sleep
+systemctl suspend
