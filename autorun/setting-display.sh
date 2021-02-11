6faaa9871fa0a5827be52a1feccfaaf2e01496ce
ichristmas <christmas.hack9@gmail.com>
2021-02-11 19:20:17 +0700

    [Added] autorun

---
+++ autorun/setting-display.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+nvidia-settings --assign CurrentMetaMode="HDMI-0: 1920x1080_60 +0+180 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-0: 1920x1080_144 +1920+0 {viewportin=2560x1440, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
