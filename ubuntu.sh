7cbbaa0cbdb76df63b00742b93f535c8e1abdf68
ichristmas <42145020+ichristmas@users.noreply.github.com>
2022-01-22 11:17:08 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -529,6 +529,9 @@ git config --global pull.rebase false
 ssh-keygen -t rsa -b 4096 -C ""
 cat .ssh/id_rsa.pub|c
 
+# config time dual boot
+timedatectl set-local-rtc 1
+
 # config display
 sudo cvt 1920 1080
 sudo xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
